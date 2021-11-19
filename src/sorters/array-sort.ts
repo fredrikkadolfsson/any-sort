ff4921a16b425397dc30390a5ea029ef0e5708dc
Fredrik Adolfsson <fredrikkadolfsson@gmail.com>
2021-11-19 23:46:33 +0100

    chore: bump dependencies

--- src/sorters/array-sort.ts
+++ src/sorters/array-sort.ts
@@ -3,6 +3,6 @@ import { Array_ } from "../types";
 import { trueSort } from "./true-sort";
 
 export const arraySort = <T = Array_>(array: T): T =>
-  (((array as unknown) as Array_)
+  (array as unknown as Array_)
     .map((a) => trueSort(a))
-    .sort((a, b) => unknownCompare(a, b, 0)) as unknown) as T;
+    .sort((a, b) => unknownCompare(a, b, 0)) as unknown as T;
