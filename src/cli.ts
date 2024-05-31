e4b405d49a9411bf716138d02a9a56a19a876c51
Fredrik Adolfsson <fredrikkadolfsson@gmail.com>
2024-05-31 13:56:26 +0200

    fix(cli): add default key

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,15 @@
-import fs from "fs";
-import trueSort from ".";
+import { promises as fs } from "fs";
 import type { UnknownObject } from "./types";
 import { parseArguments } from "./utils/cli-arguments";
+import trueSort from ".";
 
 const init = async () => {
   const { path, options } = parseArguments();
 
-  const unsortedJson = (await import(path)) as UnknownObject;
+  const data = await fs.readFile(path, "utf8");
+  const unsortedJson = JSON.parse(data) as UnknownObject;
   const sortedJson = trueSort(unsortedJson, options);
-  fs.writeFileSync(path, JSON.stringify(sortedJson, undefined, 2));
+  await fs.writeFile(path, JSON.stringify(sortedJson, undefined, 2));
 };
 
 void init();
